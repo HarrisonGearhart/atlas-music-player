710f76cb540e44bc065e7f37e3a104eba4dd0551
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-09-25 01:07:50 -0500

    fixed colors, app complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
 import Footer from "./Footer";
 import MusicPlayer from "./MusicPlayer";
 import DarkModeToggle from "./DarkModeToggle";
-import LoadingSkeleton from "./LoadingSkeleton";
 
 function App() {
   return (
-    <div className="h-full flex flex-col justify-between p-8 min-h-screen bg-[var(--color-midnight)] text-[var(--color-text)]">
+    <div
+      className="h-full flex flex-col justify-between p-8 min-h-screen bg-[var(--color-midnight)] text-[var(--color-text)] transition-colors duration-300"
+    >
       {/* Dark mode toggle in top-right corner */}
       <div className="flex justify-end mb-4">
         <DarkModeToggle />
       </div>
 
       <MusicPlayer />
+
       <Footer />
     </div>
   );
